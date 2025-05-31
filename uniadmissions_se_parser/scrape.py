cfaffa93997547573b0f9036d490f822c9265526
Dimuthu Gunawardhana <dimuthukag@gmail.com>
2025-05-31 20:54:36 +0530

    style(scrape): fix direct call of self.__courseKeyword

--- uniadmissions_se_parser/scrape.py
+++ uniadmissions_se_parser/scrape.py
@@ -56,7 +56,7 @@ class Query:
         self.build()
     
     def build(self)->None:
-        self.__queryString=f'{self.__courseKeyword}{self.queryCourseType}{self.queryCourseStudyPace}{self.queryCourseLanguage}{self.queryCourseLevel}'
+        self.__queryString=f'{self.queryCourseKeyword}{self.queryCourseType}{self.queryCourseStudyPace}{self.queryCourseLanguage}{self.queryCourseLevel}'
     
     @property
     def get(self)->str:

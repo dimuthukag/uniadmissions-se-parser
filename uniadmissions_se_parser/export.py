070c9d2a2d7fd8ec79e532bacf4d62f95b42d387
Dimuthu Gunawardhana <dimuthukag@gmail.com>
2025-06-01 13:27:05 +0530

    feat(export): modify output to csv file export

--- uniadmissions_se_parser/export.py
+++ uniadmissions_se_parser/export.py
@@ -7,18 +7,13 @@ class Exporter:
         self.__exportFileName = exportFileName
 
     def toCsv(self)->None:
-        if not self.__courseList:
-                return  # Handle empty list case
-
-        fieldnames = self.__courseList[0].keys()  # Get keys from the first dictionary
-
-        with open(f'{self.__exportFileName}.csv', 'w', newline='', encoding='utf-8') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()  # Write the column headers
-            writer.writerows(self.__courseList)  # Write the data rows
+        if not self.__exportFileName:
+            return
+        df = pd.DataFrame(self.__courseList)
+        df.to_csv(f'{self.__exportFileName}.csv', index=False)
 
     def toExcel(self)->None:
-         if not self.__courseList:
-              return
-         df = pd.DataFrame(self.__courseList)
-         df.to_excel(f'{self.__exportFileName}.xlsx',index=False)
+        if not self.__courseList:
+            return
+        df = pd.DataFrame(self.__courseList)
+        df.to_excel(f'{self.__exportFileName}.xlsx',index=False)

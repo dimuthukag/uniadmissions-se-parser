73b39c05985756d0730dbbd857564b827d34d45e
Dimuthu Gunawardhana <dimuthukag@gmail.com>
2025-06-01 13:37:44 +0530

    refactor(export): remove the repetition of pandas dataframe variable use

--- uniadmissions_se_parser/export.py
+++ uniadmissions_se_parser/export.py
@@ -4,15 +4,14 @@ class Exporter:
     def __init__(self,courseList:list,exportFileName:str='output'):
         self.__courseList=courseList
         self.__exportFileName = exportFileName
+        self.__dataFrame = pd.DataFrame(self.__courseList)
 
     def toCsv(self)->None:
         if not self.__exportFileName:
             return
-        df = pd.DataFrame(self.__courseList)
-        df.to_csv(f'{self.__exportFileName}.csv', index=False)
+        self.__dataFrame.to_csv(f'{self.__exportFileName}.csv', index=False)
 
     def toExcel(self)->None:
         if not self.__courseList:
             return
-        df = pd.DataFrame(self.__courseList)
-        df.to_excel(f'{self.__exportFileName}.xlsx',index=False)
+        self.__dataFrame.to_excel(f'{self.__exportFileName}.xlsx',index=False)
